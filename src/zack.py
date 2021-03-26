21d4d656f5de254db86908e7fac3eb40cf7a6b3f
ZackStrater <zack.m.strater@gmail.com>
2021-03-26 14:52:08 -0700

    lemmatize

--- src/zack.py
+++ src/zack.py
@@ -8,6 +8,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 import numpy as np
 import matplotlib.pyplot as plt
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
 
 # data = pd.read_json('../data/ufodata.json.zip', lines=True)
 # print(data.shape)
@@ -52,41 +56,51 @@ import matplotlib.pyplot as plt
 # print(df.shape)
 df = pd.read_csv('../data/cleaned_ufo.csv')
 df['state'] = df['location'].str[-2:]
-print(df.columns)
-print(df['shape'].value_counts().head(10))
-print(df['occurred'].value_counts().head(10))
-print(df['location'].value_counts().head(10))
-print(df['state'].value_counts().head(10))
 
-def plot_bar(ax, data, labels, title):
+def plot_bar(ax, data, labels, title, x_axis, y_axis):
     x = np.arange(len(data))
     bars = ax.bar(x, data)
     ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.set_title(title)
+    ax.set_xticklabels(labels, size = 15)
+    ax.set_xlabel(x_axis, size=20)
+    ax.set_ylabel(y_axis, size=20)
+    ax.set_title(title, size=25)
 
 shape = df['shape'].value_counts().head(10)
-shape.index[2] = 'other'
+state = df['state'].value_counts().head(11)
 
 
-
-# fig, ax = plt.subplots()
-# plot_bar(ax, shape, shape.index, 'Most Common Shapes')
+# fig, ax = plt.subplots(figsize=(16, 8))
+# plot_bar(ax, shape, shape.index, 'Most Common UFO Shapes', 'Shape', 'Count')
 # plt.show()
 
-# tfidf = TfidfVectorizer(stop_words='english', strip_accents='ascii', max_features=1000, ngram_range=(1,1))
-# X = tfidf.fit_transform(df['contents'].values).todense()
-# features = np.array(tfidf.get_feature_names())
 #
-#
-# nmf = NMF(n_components=10, max_iter=1000)
-# nmf.fit(X)
-# W = nmf.transform(X)
-# H = nmf.components_
-# error = nmf.reconstruction_err_
-# print('reconstruction error:', nmf.reconstruction_err_)
-#
-# sorted = np.array(np.argsort(H, axis=1))
-# top_ten_words = sorted[:, 990:]
-# for row in top_ten_words:
-#     print(features[row])
+# fig, ax = plt.subplots(figsize=(16, 8))
+# indexes = state.index.tolist()
+# indexes[1] = 'NA'
+# plot_bar(ax, state, indexes, 'States With Highest UFO Density', 'State', 'Count')
+# plt.show()
+
+
+word_tokens = [word_tokenize(document) for document in df['contents']]
+lemmatizer = WordNetLemmatizer()
+tokens_lemmatize = [list(map(lemmatizer.lemmatize, sent)) for sent in word_tokens]
+tfidf_input = [' '.join(words) for words in tokens_lemmatize]
+
+
+tfidf = TfidfVectorizer(stop_words='english', strip_accents='ascii', max_features=1000, ngram_range=(1,1))
+X = tfidf.fit_transform(tfidf_input).todense()
+features = np.array(tfidf.get_feature_names())
+
+
+nmf = NMF(n_components=10, max_iter=1000)
+nmf.fit(X)
+W = nmf.transform(X)
+H = nmf.components_
+error = nmf.reconstruction_err_
+print('reconstruction error:', nmf.reconstruction_err_)
+
+sorted = np.array(np.argsort(H, axis=1))
+top_ten_words = sorted[:, 990:]
+for row in top_ten_words:
+    print(features[row])
